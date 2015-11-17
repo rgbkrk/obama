849101cd4e474fc87f9c53aa8fd638f8327740f3
David Konsumer <konsumer@jetboystudio.com>
2015-11-17 12:55:13 -0800

    added ansi seal and non-endorsement message

--- bin/cmd.js
+++ bin/cmd.js
@@ -2,3 +2,11 @@
 
 var opts = require('../options.js')
 require('standard-engine').cli(opts)
+
+process.on('exit', function (status) {
+  if (status === 0) {
+    console.log(require('fs').readFileSync(__dirname + '/../seal.ans').toString())
+  }else{
+    console.log('The president cannot endorse this code.')
+  }
+})
